6bda04b6d8028d769253439a3b50b657e025dbfb
cowlike <cowlike@gmail.com>
2014-10-05 11:21:30 -0500

    add parameterized resource example

--- src/liberator_tutorial/core.clj
+++ src/liberator_tutorial/core.clj
@@ -3,9 +3,22 @@
             [ring.middleware.params :refer [wrap-params]]
             [compojure.core :refer [defroutes ANY]]))
 
+;; define a simple parameterized resource
+
+(defresource parameter [txt]
+  :available-media-types ["text/plain"]
+  :handle-ok (fn [_] (format "The text is %s" txt)))
+
+;; set up routes to our resources
+
 (defroutes app
   (ANY "/foo" [] (resource :available-media-types ["text/html"]
-                           :handle-ok "<html>Hello, Internet.</html>")))
+                           :handle-ok "<html>Hello, Internet.</html>"))
+  (ANY "/time" [] (resource :available-media-types ["text/html"]
+                           :handle-ok (fn [ctx]
+                                        (format "<html>It's %d milliseconds since the beginning of the epoch."
+                                                (System/currentTimeMillis)))))
+  (ANY "/foo/:txt" [txt] (parameter txt)))
 
 (def handler 
   (-> app wrap-params))
